2fca6000d0fd3d366d3a092774766e2172f95eca
Gheorghe-Cristian CREȚAN (101605) <gheorghe.cretan@stud.acs.upb.ro>
2022-06-05 15:33:23 +0300

    Modified database with packets in a separate table

--- src/snortdb.c
+++ src/snortdb.c
@@ -2,18 +2,94 @@
 * @Author: Cristi Cretan
 * @Date:   2022-03-26 16:52:22
 * @Last Modified by:   Cristi Cretan
-* @Last Modified time: 2022-03-27 22:12:26
+* @Last Modified time: 2022-06-05 15:31:00
 */
 #include "snortdb.h"
 #include <string.h>
 #include <stdio.h>
 
+#define QUERY_LEN 4096+512
 #define TO_IP(x) x >> 24, (x >> 16) & 0xff, (x >> 8) & 0xff, x & 0xff
 #define IT_OR_NULL(x) strlen(x) == 0 ? "NULL" : x
 // #define IT_OR_ZERO(x) x == NULL ? 0 : x
+#define BUFLEN 4096
 
 static long s_pos = 0, s_off = 0;
-static long last_event_id = 0;
+static long last_event_id = 1;
+
+static off_t filesize;
+struct stat finfo;
+
+int get_record(u2iterator *it, u2record *record) {
+    uint32_t bytes_read;
+    uint8_t *tmp;
+
+    if(!it || !it->file) return FAILURE;
+
+    /* check if the log was rotated */
+    if(feof(it->file)) {
+        /* Get next timestamped file? */
+        puts("Hit the EOF .. and this is not being handled yet.");
+        return FAILURE;
+    }
+
+    if ( s_off )
+    {
+        if (fseek(it->file, s_pos+s_off, SEEK_SET)) 
+        {
+            puts("Unable to SEEK on current file .. and this is not being handled yet.");
+            return FAILURE;
+        }
+        s_off = 0;
+    }
+
+    /* read type and length */
+    bytes_read = fread(record, 1, sizeof(uint32_t) * 2, it->file);
+    /* But they're in network order! */
+    record->type= ntohl(record->type);
+    record->length= ntohl(record->length);
+
+    if(record->type == UNIFIED2_PACKET) record->length+=4;
+
+    if(bytes_read == 0)
+       /* EOF */
+       return FAILURE;
+
+    if(bytes_read != sizeof(uint32_t)*2) {
+        puts("get_record: (1) Failed to read all of record.");
+        printf("\tRead %u of %lu bytes\n", bytes_read, (unsigned long)sizeof(uint32_t)*2);
+        return FAILURE;
+    }
+
+    s_pos = ftell(it->file);
+
+    tmp = (uint8_t *)realloc(record->data, record->length);
+    
+    if (!tmp)
+    {
+        puts("get_record: (2) Failed to allocate memory.");
+        free(record->data);
+        return FAILURE;
+    }
+
+    record->data = tmp;
+
+    bytes_read = fread(record->data, 1, record->length, it->file);
+
+    if(bytes_read != record->length) {
+        puts("get_record: (3) Failed to read all of record data.");
+        printf("\tRead %u of %u bytes\n", bytes_read, record->length);
+
+        if ( record->type != UNIFIED2_PACKET || 
+            bytes_read < ntohl(((Serial_Unified2Packet*)record->data)->packet_length)
+        )
+            return FAILURE;
+
+        clearerr(it->file);
+    }
+
+    return SUCCESS;
+}
 
 void finish_with_error(MYSQL *con)
 {
@@ -49,9 +125,27 @@ static void event3_to_db(u2record *record, MYSQL *con)
     }
     /* done changing the network ordering */
 
-    char query[512];
+    char query[QUERY_LEN];
 
-    memset(query, 0, 512);
+    if (last_event_id != 1) {
+        if (mysql_query(con, "SELECT MAX(Pk_Event_Id) from snortdb.events")) {
+            finish_with_error(con);
+        }
+
+        MYSQL_RES *result = mysql_store_result(con);
+
+        if (result == NULL) {
+            finish_with_error(con);
+        }
+
+        MYSQL_ROW row = mysql_fetch_row(result);
+        last_event_id = atoi(row[0]);
+        mysql_free_result(result);
+    }
+
+    printf("dport_icode = %u\n", event.dport_icode);
+
+    memset(query, 0, QUERY_LEN);
 
     sprintf(query,
         "INSERT INTO events (sensor_id, event_id, event_second, event_microsecond, signature_id, "
@@ -63,7 +157,7 @@ static void event3_to_db(u2record *record, MYSQL *con)
         "%u, %u, %u, %u, "
         "%u, \"%u.%u.%u.%u\", \"%u.%u.%u.%u\", "
         "%u, %u, %u, %u, %u, "
-        "%u, %u, %u, %s, 1)",
+        "%u, %u, %u, %s, %lu)",
         event.sensor_id, event.event_id,
         event.event_second, event.event_microsecond,
         event.signature_id, event.generator_id,
@@ -72,32 +166,255 @@ static void event3_to_db(u2record *record, MYSQL *con)
         TO_IP(event.ip_destination), event.sport_itype,
         event.dport_icode, event.protocol,
         event.impact_flag, event.blocked,
-        event.mpls_label, event.vlanId, event.pad2, IT_OR_NULL(event.app_name));
+        event.mpls_label, event.vlanId, event.pad2, IT_OR_NULL(event.app_name), last_event_id);
 
-    // printf("query: %s\n\n", query);
     if (mysql_query(con, query)) {
         finish_with_error(con);
     }
 
-    if (mysql_query(con, "SELECT MAX(Pk_Event_Id) from snortdb.events")) {
-        finish_with_error(con);
+    return;
+}
+
+static void event6_to_db(u2record *record, MYSQL *con)
+{
+    printf("event6_to_db\n");
+    uint8_t *field;
+    int i;
+    Serial_Unified2IDSEventIPv6_legacy event;
+    char ip6buf_source[INET6_ADDRSTRLEN+1];
+    char ip6buf_dest[INET6_ADDRSTRLEN+1];
+
+    memcpy(&event, record->data, sizeof(Serial_Unified2IDSEventIPv6_legacy));
+
+    /* network to host ordering */
+    /* In the event structure, only the last 40 bits are not 32 bit fields */
+    /* The first fields need to be convertted */
+    field = (uint8_t*)&event;
+    for(i=0; i<9; i++, field+=4) {
+        *(uint32_t*)field = ntohl(*(uint32_t*)field);
     }
 
-    MYSQL_RES *result = mysql_store_result(con);
+    field = field + 2*sizeof(struct in6_addr);
 
-    if (result == NULL) {
-        finish_with_error(con);
+    /* last 3 fields, with the exception of the last most since it's just one byte */
+    *(uint16_t*)field = ntohs(*(uint16_t*)field); /* sport_itype */
+    field += 2;
+    *(uint16_t*)field = ntohs(*(uint16_t*)field); /* dport_icode */
+    /* done changing the network ordering */
+
+    inet_ntop(AF_INET6, &event.ip_source, ip6buf_source, INET6_ADDRSTRLEN);
+    inet_ntop(AF_INET6, &event.ip_destination, ip6buf_dest, INET6_ADDRSTRLEN);
+
+    char query[QUERY_LEN];
+
+    if (last_event_id != 1) {
+        if (mysql_query(con, "SELECT MAX(Pk_Event_Id) from snortdb.events")) {
+            finish_with_error(con);
+        }
+
+        MYSQL_RES *result = mysql_store_result(con);
+
+        if (result == NULL) {
+            finish_with_error(con);
+        }
+
+        MYSQL_ROW row = mysql_fetch_row(result);
+        last_event_id = atoi(row[0]);
+        mysql_free_result(result);
     }
 
-    MYSQL_ROW row = mysql_fetch_row(result);
-    last_event_id = atoi(row[0]);
-    mysql_free_result(result);
+    memset(query, 0, QUERY_LEN);
+
+    sprintf(query,
+        "INSERT INTO events (sensor_id, event_id, event_second, event_microsecond, signature_id, "
+        "generator_id, signature_revision, classification_id, "
+        "priority_id, ip_source, ip_destination, sport_itype, "
+        "dport_icode, protocol, impact_flag, blocked, rules_Pk_Rule_Id) "
+        "VALUES (%u, %u, %u, %u, "
+        "%u, %u, %u, %u, "
+        "%u, \"%s\", \"%s\", "
+        "%u, %u, %u, %u, %u, %lu)",
+        event.sensor_id, event.event_id,
+        event.event_second, event.event_microsecond,
+        event.signature_id, event.generator_id,
+        event.signature_revision, event.classification_id,
+        event.priority_id, ip6buf_source,
+        ip6buf_dest, event.sport_itype,
+        event.dport_icode, event.protocol,
+        event.impact_flag, event.blocked,
+        last_event_id);
+
+    if (mysql_query(con, query)) {
+        finish_with_error(con);
+    }
 
     return;
 }
 
-#define BUFLEN 4096
-#define QUERY_LEN 4096+512
+static void event2_6_to_db(u2record *record, MYSQL *con)
+{
+    printf("event2_6_to_db\n");
+    uint8_t *field;
+    int i;
+    char ip6buf_source[INET6_ADDRSTRLEN+1];
+    char ip6buf_dest[INET6_ADDRSTRLEN+1];
+    Serial_Unified2IDSEventIPv6 event;
+
+    memcpy(&event, record->data, sizeof(Serial_Unified2IDSEventIPv6));
+
+    /* network to host ordering */
+    /* In the event structure, only the last 40 bits are not 32 bit fields */
+    /* The first fields need to be convertted */
+    field = (uint8_t*)&event;
+    for(i=0; i<9; i++, field+=4) {
+        *(uint32_t*)field = ntohl(*(uint32_t*)field);
+    }
+
+    field = field + 2*sizeof(struct in6_addr);
+
+    /* last 3 fields, with the exception of the last most since it's just one byte */
+    *(uint16_t*)field = ntohs(*(uint16_t*)field); /* sport_itype */
+    field += 2;
+    *(uint16_t*)field = ntohs(*(uint16_t*)field); /* dport_icode */
+    field +=6;
+    *(uint32_t*)field = ntohl(*(uint32_t*)field); /* mpls_label */
+    field += 4;
+    /* policy_id and vlanid */
+    for(i=0; i<2; i++, field+=2) {
+        *(uint16_t*)field = ntohs(*(uint16_t*)field);
+    }
+    /* done changing the network ordering */
+
+    inet_ntop(AF_INET6, &event.ip_source, ip6buf_source, INET6_ADDRSTRLEN);
+    inet_ntop(AF_INET6, &event.ip_source, ip6buf_dest, INET6_ADDRSTRLEN);
+
+    char query[QUERY_LEN];
+
+    if (last_event_id != 1) {
+        if (mysql_query(con, "SELECT MAX(Pk_Event_Id) from snortdb.events")) {
+            finish_with_error(con);
+        }
+
+        MYSQL_RES *result = mysql_store_result(con);
+
+        if (result == NULL) {
+            finish_with_error(con);
+        }
+
+        MYSQL_ROW row = mysql_fetch_row(result);
+        last_event_id = atoi(row[0]);
+        mysql_free_result(result);
+    }
+
+    memset(query, 0, QUERY_LEN);
+
+    sprintf(query,
+        "INSERT INTO events (sensor_id, event_id, event_second, event_microsecond, signature_id, "
+        "generator_id, signature_revision, classification_id, "
+        "priority_id, ip_source, ip_destination, sport_itype, "
+        "dport_icode, protocol, impact_flag, blocked, mpls_label, "
+        "vlanId, pad2, rules_Pk_Rule_Id) "
+        "VALUES (%u, %u, %u, %u, "
+        "%u, %u, %u, %u, "
+        "%u, \"%s\", \"%s\", "
+        "%u, %u, %u, %u, %u, %u, %u, %u, %lu)",
+        event.sensor_id, event.event_id,
+        event.event_second, event.event_microsecond,
+        event.signature_id, event.generator_id,
+        event.signature_revision, event.classification_id,
+        event.priority_id, ip6buf_source,
+        ip6buf_dest, event.sport_itype,
+        event.dport_icode, event.protocol,
+        event.impact_flag, event.blocked, event.mpls_label, event.vlanId,
+        event.pad2, last_event_id);
+
+    if (mysql_query(con, query)) {
+        finish_with_error(con);
+    }
+}
+
+static void event3_6_to_db(u2record *record, MYSQL *con)
+{
+    printf("event3_6_to_db\n");
+    uint8_t *field;
+    int i;
+    char ip6buf_source[INET6_ADDRSTRLEN+1];
+    char ip6buf_dest[INET6_ADDRSTRLEN+1];
+    Serial_Unified2IDSEventIPv6 event;
+
+    memcpy(&event, record->data, sizeof(Serial_Unified2IDSEventIPv6));
+
+    /* network to host ordering */
+    /* In the event structure, only the last 40 bits are not 32 bit fields */
+    /* The first fields need to be convertted */
+    field = (uint8_t*)&event;
+    for(i=0; i<9; i++, field+=4) {
+        *(uint32_t*)field = ntohl(*(uint32_t*)field);
+    }
+
+    field = field + 2*sizeof(struct in6_addr);
+
+    /* last 3 fields, with the exception of the last most since it's just one byte */
+    *(uint16_t*)field = ntohs(*(uint16_t*)field); /* sport_itype */
+    field += 2;
+    *(uint16_t*)field = ntohs(*(uint16_t*)field); /* dport_icode */
+    field +=6;
+    *(uint32_t*)field = ntohl(*(uint32_t*)field); /* mpls_label */
+    field += 4;
+    /* policy_id and vlanid */
+    for(i=0; i<2; i++, field+=2) {
+        *(uint16_t*)field = ntohs(*(uint16_t*)field);
+    }
+    /* done changing the network ordering */
+
+    inet_ntop(AF_INET6, &event.ip_source, ip6buf_source, INET6_ADDRSTRLEN);
+    inet_ntop(AF_INET6, &event.ip_source, ip6buf_dest, INET6_ADDRSTRLEN);
+
+    char query[QUERY_LEN];
+
+    if (last_event_id != 1) {
+        if (mysql_query(con, "SELECT MAX(Pk_Event_Id) from snortdb.events")) {
+            finish_with_error(con);
+        }
+
+        MYSQL_RES *result = mysql_store_result(con);
+
+        if (result == NULL) {
+            finish_with_error(con);
+        }
+
+        MYSQL_ROW row = mysql_fetch_row(result);
+        last_event_id = atoi(row[0]);
+        mysql_free_result(result);
+    }
+
+    memset(query, 0, QUERY_LEN);
+
+    sprintf(query,
+        "INSERT INTO events (sensor_id, event_id, event_second, event_microsecond, signature_id, "
+        "generator_id, signature_revision, classification_id, "
+        "priority_id, ip_source, ip_destination, sport_itype, "
+        "dport_icode, protocol, impact_flag, blocked, mpls_label, "
+        "vlanId, pad2, app_name, rules_Pk_Rule_Id) "
+        "VALUES (%u, %u, %u, %u, "
+        "%u, %u, %u, %u, "
+        "%u, \"%s\", \"%s\", "
+        "%u, %u, %u, %u, %u, "
+        "%u, %u, %u, %s, %lu)",
+        event.sensor_id, event.event_id,
+        event.event_second, event.event_microsecond,
+        event.signature_id, event.generator_id,
+        event.signature_revision, event.classification_id,
+        event.priority_id, ip6buf_source,
+        ip6buf_dest, event.sport_itype,
+        event.dport_icode, event.protocol,
+        event.impact_flag, event.blocked,
+        event.mpls_label, event.vlanId, event.pad2, IT_OR_NULL(event.app_name), last_event_id);
+
+    if (mysql_query(con, query)) {
+        finish_with_error(con);
+    }
+}
 
 static void packet_to_db(u2record *record, MYSQL *con)
 {
@@ -145,11 +462,18 @@ static void packet_to_db(u2record *record, MYSQL *con)
 
     char query[QUERY_LEN];
     sprintf(query, "INSERT INTO packets (sensor_id, event_id, event_second, packet_second, packet_microsecond, "
-            "linktype, packet_length, packet_data, events_Pk_Event_Id)"
-            "VALUES (%u, %u, %u, %u, %u, %u, %u, x'%s', %lu);",
+            "linktype, packet_length, events_Pk_Event_Id)"
+            "VALUES (%u, %u, %u, %u, %u, %u, %u, %lu);",
                     packet.sensor_id, packet.event_id, packet.event_second,
                     packet.packet_second, packet.packet_microsecond, packet.linktype,
-                    packet.packet_length, hex_buf, last_event_id);
+                    packet.packet_length, last_event_id);
+
+    if (mysql_query(con, query)) {
+        finish_with_error(con);
+    }
+
+    sprintf(query, "INSERT INTO blob_packets(packet_data, packets_Pk_Packet_Id) "
+                "VALUES (x'%s', %lu);", hex_buf, last_event_id);
 
     if (mysql_query(con, query)) {
         finish_with_error(con);
