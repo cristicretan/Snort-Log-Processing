bb53f019366b69457433eea3ac607e30ce7bca93
Gheorghe-Cristian CREȚAN (101605) <gheorghe.cretan@stud.acs.upb.ro>
2022-06-26 17:59:58 +0300

    Added orderable columns in events table

--- src/server/snort.py
+++ src/server/snort.py
@@ -30,12 +30,12 @@ def execute():
 	if request.method == 'GET':
 		events_data = db.query(events)
 		all_events = db.query(events).all()
-		print(all_events)
+#		print(all_events)
 		all_dates = [datetime.utcfromtimestamp(x[3]).strftime("%Y-%m-%d") for x in all_events]
 		all_times = [datetime.utcfromtimestamp(x[4]).strftime("%H") for x in all_events]
-		print(all_times)
+#		print(all_times)
 		times = [str(x[0]) + " " + str(x[1]) for x in zip(all_dates, all_times)]
-		print(times)
+#		print(times)
 		return render_template('basic_table.html', title='Events Table', events=all_events)
 
 app.run(host='127.0.0.1', port=5000)
